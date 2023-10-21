9b67e77ed56a9b9e45fca2be7a0c4b85e6951204
Linfeng Li <linfel@umich.edu>
2023-10-21 18:01:00 -0400

    tau and transmission,  plot profile

--- tools/transmission_ck_integral.py
+++ tools/transmission_ck_integral.py
@@ -1,42 +1,84 @@
 from netCDF4 import Dataset
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# specify input file
-filenum = '1'
-filename = "cktable.lava_planet-B"+filenum+".nc"
-pwd = "/home/linfel/LavaPlanet/"
-cktable_fpath = pwd + filename
-print(f"# working on {cktable_fpath}")
+def get_band_tau_profile(filename,h,layers):
 
-# read the data
-ck_dataset = Dataset(cktable_fpath,'r')
-ck_g = ck_dataset.variables['Wavenumber'][:]
-Temperature = ck_dataset.variables['Temperature'][:]
-Pressure = ck_dataset.variables['Pressure'][:]
+    # specify input file
+    pwd = "/home/linfel/LavaPlanet/"
+    cktable_fpath = pwd + filename
+    print(f"# working on {cktable_fpath}")
 
-layers = 80                       # number of layers
-tau_layers = np.array([])         # create an empty array to store tau of each layer
+    # read the data
+    ck_dataset = Dataset(cktable_fpath,'r')
+    ck_g = ck_dataset.variables['Wavenumber'][:]
+    Temperature = ck_dataset.variables['Temperature'][:]
+    Pressure = ck_dataset.variables['Pressure'][:]
 
-for ilayer in range(layers):
-    ck_k_raw = ck_dataset.variables['SiO'][:,ilayer,1]  # k at specific pressure and temperature
-    ck_k = np.exp(ck_k_raw)/1000  # m2/mol
-    T = Temperature[ilayer]       # K
-    p = Pressure[ilayer]          # Pa
+    tau_layers = np.array([])      # create an array to store tau of each layer
+    trm_layers = np.array([])
 
-    rou = p/T/8.31446261815324    # mol/m3
-    d = 10.0/79*1000              # thickness of each layer (m) 
-    u = rou * d                   # u is constant within each layer
+    for ilayer in range(layers-1,-1,-1):
+        ck_k_raw = ck_dataset.variables['SiO'][:,ilayer,1]  # k at specific pressure and temperature
+        ck_k = np.exp(ck_k_raw)/1000  # m2/mol
+        T = Temperature[ilayer]       # K
+        p = Pressure[ilayer]          # Pa
 
-    # integrate over g space to calculate transmission
-    transmission = 0.0
-    for j in range(len(ck_g)-1):
-        k = (ck_k[j] + ck_k[j+1]) / 2
-        delta_g = ck_g[j+1] - ck_g[j]
-        transmission = transmission + np.exp(-k*u) * delta_g
+        rou = p/T/8.31446261815324    # mol/m3
+        d = h/(layers-1)*1000              # thickness of each layer (m) 
+        u = rou * d                   # u is constant within each layer
 
-    # calculate tau for this layer
-    tau = -np.log(transmission)
-    tau_layers = np.append(tau_layers,tau)
+        # integrate over g space to calculate transmission
+        trm = 0.0
+        for j in range(len(ck_g)-1):
+            k = (ck_k[j] + ck_k[j+1]) / 2
+            delta_g = ck_g[j+1] - ck_g[j]
+            trm = trm + np.exp(-k*u) * delta_g
+        trm_layers = np.append(trm_layers,trm)
+        
+        # calculate tau for this layer
+        itau = -np.log(trm)
+        tau_layers = np.append(tau_layers,itau)
+    
+    return trm_layers, tau_layers
 
-print(len(tau_layers))
-print(tau_layers)
+
+h_atm = 100.0  # thickness of atmosphere
+layers = 80
+dh = h_atm/(layers-1)
+dx = 1
+h, x = np.mgrid[0:100+dh:dh, 1:7+dx:dx]
+
+tau_all = np.empty((layers, 0))
+trm_all = np.empty((layers, 0))
+
+# get the tau profile for each band and add to the 2D array
+for filenum in list(['1','2','3','4','5','6','7']):
+
+    filename = "cktable.lava_planet-B"+filenum+".nc"
+    band_trm,band_tau = get_band_tau_profile(filename,h_atm,layers)
+    tau_all = np.hstack((tau_all, band_tau[:, np.newaxis]))
+    trm_all = np.hstack((trm_all, band_trm[:, np.newaxis]))
+
+# plot the transmission profile
+plt.figure(figsize=(10, 6))
+heatmap = plt.pcolor(x,h,trm_all,cmap='viridis')
+cb = plt.colorbar(heatmap)
+cb.set_label('transmission',size=13)
+plt.xlabel('wavenumber / $\mathrm{cm}^{-1}$',size=13)
+plt.ylabel('height / km',size=13)
+plt.tick_params(axis='both', which='major', labelsize=13)
+plt.savefig("/home/linfel/LavaPlanet/images/trm_profile.png",dpi=300)
+
+
+# plot the optical depth profile
+plt.figure(figsize=(10, 6))
+heatmap = plt.pcolor(x,h,tau_all,cmap='YlOrRd')
+cb = plt.colorbar(heatmap)
+cb.set_label('optical depth',size=13)
+plt.xlabel('wavenumber / $\mathrm{cm}^{-1}$',size=13)
+plt.ylabel('height / km',size=13)
+plt.tick_params(axis='both', which='major', labelsize=13)
+plt.savefig("/home/linfel/LavaPlanet/images/tau_profile.png",dpi=300)
+#np.savetxt("/home/linfel/LavaPlanet/outputs/tau.csv", tau_layers, delimiter=',')
